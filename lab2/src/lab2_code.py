2a9ca4c1478d820aafa4a09a2e96995dffd6f119
kmhswimgirl <163170240+kmhswimgirl@users.noreply.github.com>
2025-05-31 12:16:37 -0400

    maybe fixed a few things + added debugging

--- lab2/src/lab2_code.py
+++ lab2/src/lab2_code.py
@@ -10,7 +10,7 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, PoseStamped, TwistStamped
 
 
-class Lab2(Node):
+class Lab2:
 
     def __init__(self, px, py, pth):
         # Constructor
@@ -32,6 +32,8 @@ class Lab2(Node):
 
 
     def send_speed(self, linear_speed: float, angular_speed: float):
+        self.get_logger().info("starting send_speed")
+
         msg_cmd_vel = Twist()
 
         msg_cmd_vel.linear.x = linear_speed
@@ -42,9 +44,11 @@ class Lab2(Node):
         msg_cmd_vel.angular.y = 0.0
         msg_cmd_vel.angular.z = angular_speed
 
-        #self.cmd_vel.publish(msg_cmd_vel)
+        self.cmd_vel.publish(msg_cmd_vel)
+        self.get_logger().info("ending send_speed")
 
     def drive(self, distance: float, linear_speed: float):
+        self.get_logger().info("starting drive")
 
         error = 1000
         tol = 0.05
@@ -56,51 +60,58 @@ class Lab2(Node):
             self.send_speed(linear_speed,0.0)
             error = distance - dist_traveled
 
-
-        self.send_speed(0.0,0.0) # stop robot when loop is complete (i.e. distance is below tolerance)
+        self.send_speed(0.0, 0.0) # stop robot when loop is complete (i.e. distance is below tolerance)
+        self.get_logger().info("ending drive")
 
     def rotate(self, target_heading: float, ang_speed: float):
-        tolerance = 0.05  # radians
-        def normalize_angle(angle):
-            while angle > math.pi:
-                angle -= 2 * math.pi
-            while angle < -math.pi:
-                angle += 2 * math.pi
-            return angle
+        self.get_logger().info("starting rotation")
+        tol= 0.05  # radians
+        goal_th = target_heading
+        ang_err = 1000
 
-        while rclpy.ok():
-            error = normalize_angle(target_heading - self.pth)
-            if abs(error) < tolerance:
-                break
-            direction = 1.0 if error > 0 else -1.0
-            #self.send_speed(0.0, direction * abs(ang_speed))
-            rclpy.spin_once(self, timeout_sec=0.01)
-        #self.send_speed(0.0, 0.0)
+        if goal_th < 0 and abs(goal_th) > math.pi:
+            goal_th = 2*math.pi - goal_th
+
+        elif goal_th > 0 and goal_th > math.pi:
+            goal_th = goal_th - 2*math.pi
+
+        while(abs(ang_err) > tol):
+            if(goal_th < self.pth):
+                
+                self.send_speed(0.0, -ang_speed)
+            else:
+                self.send_speed(0.0, ang_speed)
+            ang_err = abs(self.pth - goal_th)
+
+        self.send_speed(0.0, 0.0)
+        self.get_logger().info("ending rotation")
 
     def go_to(self, msg: PoseStamped):
-        if not hasattr(self, 'px') or not hasattr(self, 'py') or not hasattr(self, 'pth'):
-            self.get_logger().warn("Odometry not ready, ignoring goal!")
-            return
+        self.get_logger().info("begin go_to")
 
+        #current robor position
+        init_th = self.pth
+        
+        #desired position
+        new_y = msg.pose.position.y
+        new_x = msg.pose.position.x
         init_x = self.px
         init_y = self.py
 
-        new_pos_x = msg.pose.position.x
-        new_pos_y = msg.pose.position.y
+        self.get_logger().info("go_to: variables set")
 
-        drive_heading = math.atan2((new_pos_y - init_y), (new_pos_x - init_x))
-        angle_to_turn = drive_heading - self.pth
+        #use some trig to get the new theta part of the pose (I <3 trig)
+        new_ang = math.atan2((new_y - init_y),(new_x - init_x))
+        self.rotate(new_ang, 0.5)
 
-        # Normalize to [-pi, pi]
-        while angle_to_turn > math.pi:
-            angle_to_turn -= 2 * math.pi
-        while angle_to_turn < -math.pi:
-            angle_to_turn += 2 * math.pi
+        self.get_logger().info("begin go_to")
+        time.sleep(0.05)
+        #another use of pythagorean theorem to calc distance (i think its called something Euclidian???)
+        # basically a control c, control v from drive() method && change a var name or two
+        dist =  math.sqrt((new_x - init_x)** 2 + (new_y - init_y)** 2)
+        self.drive(dist, 0.1)
 
-        self.rotate(self.pth + angle_to_turn, 0.5)
-
-        drive_dist = self.euclidean_dist(init_x, init_y, new_pos_x, new_pos_y)
-        self.drive(drive_dist, 0.1)
+        self.get_logger().info("finished go_to")
 
 
     def update_odometry(self, msg: Odometry):
@@ -140,10 +151,12 @@ class Lab2(Node):
         return output
 
     def run(self):
-        rclpy.spin(self)
+        rclpy.spin()
 
 if __name__ == '__main__':
     rclpy.init()
+
     robot = Lab2(0,0,0)
     robot.run()
+
     rclpy.get_logger().info('Driving')
